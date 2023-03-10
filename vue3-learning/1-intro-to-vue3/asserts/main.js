2be99e9574059cc580c91232ec2c0527219d1d29
TONGTONGDESHIJIE <1090060206@qq.com>
2023-03-10 09:43:25 +0800

    attribute binding && Conditional Rendering

--- vue3-learning/1-intro-to-vue3/asserts/main.js
+++ vue3-learning/1-intro-to-vue3/asserts/main.js
@@ -2,7 +2,13 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'Socks',
-            description: 'A warm fuzzy pair of socks.'
+            description: 'A warm fuzzy pair of socks.',
+            image: './images/socks_green.jpg',
+            inStock: true,
+            inventory: 100,
         }
     },
+    method:{
+
+    }
 })
